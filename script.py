9bf4a5af62ee56746a0ee31f28c4f8a8f42c2c3e
Brett Henderson <bretth18@gmail.com>
2016-01-24 21:43:56 -0800

    temporary project setbacks

--- script.py
+++ script.py
@@ -23,10 +23,13 @@ loop.start()
 #add threading events
 logged_in = threading.Event()
 
+#call connection state
+#TODO: load_dw_playlist must be defined before it is
+#       called within on_connection_state_updated
 def on_connection_state_updated(session):
     if session.connection.state is spotify.ConnectionState.LOGGED_IN:
         logged_in.set()
-        load_dw_playlist()
+        #load_dw_playlist()
 
 #register event listeners
 session.on(
@@ -49,30 +52,56 @@ logged_in.wait()
 #define sessionUser
 session.user
 
-def load_dw_playlist(dwPlaylist):
-    #call input for dwPlaylist
-    dwPlaylist = raw_input("input discover weekly playlist URI: ")
+dwPlaylist = 'spotify:user:spotifydiscover:playlist:6Rrm3dKWoece7l3mtkM2UJ'
+playlist = session.get_playlist(dwPlaylist)
+playlist.tracks
 
-    #load DW playlist
-    len(session.playlist_container)
-    #playlist = session.playlist_container[0]
-    #playlist.load(dwPlaylist)
-    playlist = session.get_playlist(dwPlaylist)
-    container.insert(1, playlist)
+#append playlist tracks to empty array
+#UNECESSARY CODE BOI
+trackStack = []
+trackStack.append(playlist.tracks)
+
+def archive_tracks():
+#create container to store our new archive playlist
+    archivePlaylist = session.get_playlist('spotify:user:bretth18:playlist:7wAo4Tv2vCOHr8l1hzG7Xn')
+    archivePlaylist.add_tracks(playlist.tracks)
+    archivePlaylist.tracks
+
+archive_tracks()
+
+
+
+
+
+
+
+
+
+# def load_dw_playlist(dwPlaylist):
+#     #call input for dwPlaylist
+#     #playlist is set for testing purposes!
+#     dwPlaylist = 'spotify:user:spotifydiscover:playlist:6Rrm3dKWoece7l3mtkM2UJ'
+#
+#     #load DW playlist
+#     len(session.playlist_container)
+#     #playlist = session.playlist_container[0]
+#     #playlist.load(dwPlaylist)
+#     playlist = session.get_playlist(dwPlaylist)
+#     container.insert(1, playlist)
 
 
 #store playlist.tracks in an array to copy to new playlist
 
-    if playlist.is_loaded = True:
-        playlist.tracks[0]
-
-def tracks_added(playlist, tracks, index):
-    container.insert(1, playlist.tracks)
-    
-    print('dw tracks added to archive')
-
-def container_loaded(playlist_container):
-    print('Playlist container loaded...')
+#     if playlist.is_loaded == True:
+#         playlist.tracks[0]
+#
+# def tracks_added(playlist, tracks, index):
+#     container.insert(1, playlist.tracks)
+#
+#     print('dw tracks added to archive')
+#
+# def container_loaded(playlist_container):
+#     print('Playlist container loaded...')
 
 
 #show subs option
